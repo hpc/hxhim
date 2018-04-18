0fea54e33603112423034a3cac281b7bacdbe8d3
Jason Lee <jasonlee@lanl.gov>
2018-04-18 17:53:39 -0600

    Added argument parser to random.sh

--- examples/random.sh
+++ examples/random.sh
@@ -6,9 +6,78 @@ MIN_VALUE_LEN=1
 MAX_VALUE_LEN=10
 COUNT=10
 RANKS=10
-MDHIM_CONFIG="mdhim.conf"
+MDHIM_CONFIG=${MDHIM_CONFIG:-"mdhim.conf"}
 
-# Generate key value pairs
+function help() {
+    echo "Usage: $(basename $0) [Options] PUT|GET|DEL|BPUT|BGET|BDEL ..."
+    echo
+    echo "    Options:"
+    echo "        -h, --help       show help"
+    echo "        -n, --count      number of key value pairs"
+    echo "        -np, --ranks     the number of MPI ranks"
+    echo "        --min_key_len    the minimum length of a key"
+    echo "        --max_key_len    the maximum length of a key"
+    echo "        --min_value_len  the minimum length of a value"
+    echo "        --max_value_len  the maximum length of a value"
+}
+
+# Parse command line arguments
+# https://stackoverflow.com/a/14203146
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -h|--help)
+    help
+    exit 0
+    ;;
+    -n|--count)
+    COUNT=$2
+    shift # past argument
+    shift # past value
+    ;;
+    -np|--ranks)
+    RANKS=$2
+    shift # past argument
+    shift # past value
+    ;;
+    --min_key_len)
+    MIN_KEY_LEN=$2
+    shift # past argument
+    shift # past value
+    ;;
+    --max_key_len)
+    Max_KEY_LEN=$2
+    shift # past argument
+    shift # past value
+    ;;
+    --min_value_len)
+    MIN_VALUE_LEN=$2
+    shift # past argument
+    shift # past value
+    ;;
+    --max_value_len)
+    Max_VALUE_LEN=$2
+    shift # past argument
+    shift # past value
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+# Check for arguments
+if [[ "$#" -eq "0" ]] ; then
+    help
+    exit 0
+fi
+
+# Generate key valuepairs
 declare -A KEYPAIRS
 for i in $(seq 1 $COUNT); do
     KEYPAIRS[$(pwgen $((MIN_KEY_LEN+RANDOM%MAX_KEY_LEN)) 1)]=$(pwgen $((MIN_VALUE_LEN+RANDOM%MAX_VALUE_LEN)) 1)
@@ -38,13 +107,10 @@ OPS["BDEL"]=$(printf "BDELETE $COUNT";
                   printf " $key"
               done)
 
-# delete old database
-rm -rf mdhimTstDB--0-* mdhim_manifest*
-
 # run the CLI
 export MDHIM_CONFIF=$MDHIM_CONFIG
 (
-    for op in "$@"; do
+    for op in "${POSITIONAL[@]}"; do
         printf "${OPS[$op]}\n"
     done
 ) | mpirun -np $RANKS examples/cli
