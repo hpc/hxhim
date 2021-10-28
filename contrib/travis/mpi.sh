d60fdfeb40fc22c44856e61555654e3895e6912d
Jason Lee <jasonlee@lanl.gov>
2021-10-28 09:38:34 -0600

    update mpi to use latest from github

--- contrib/travis/mpi.sh
+++ contrib/travis/mpi.sh
@@ -2,23 +2,24 @@
 
 if [[ "$#" -lt 3 ]]
 then
-    echo "Syntax: $0 DL_DIR BUILD_NAME INSTALL_DIR" 1>&2
+    echo "Syntax: $0 DL_DIR BUILD_NAME INSTALL_DIR [PROCS]" 1>&2
     exit 1
 fi
 
 DL_DIR="$1"
 BUILD_NAME="$2"
 INSTALL_DIR="$3"
+PROCS="$4"
 
 cd "${DL_DIR}"
-wget https://download.open-mpi.org/release/open-mpi/v4.0/openmpi-4.0.5.tar.bz2
-tar -xf openmpi-4.0.5.tar.bz2
-cd openmpi-4.0.5
+git clone --recurse-submodules --depth 1 https://github.com/open-mpi/ompi.git
+cd ompi
+./autogen.pl
 mkdir "${BUILD_NAME}"
 cd "${BUILD_NAME}"
-MPI_PREFIX="${INSTALL_DIR}/mpi"
-../configure --prefix="${MPI_PREFIX}"
-make -j $(nproc --all)
-make -j $(nproc --all) install
+MPI_PREFIX="${INSTALL_DIR}/openmpi-github"
+../configure --prefix="${MPI_PREFIX}" --disable-man-pages --enable-threads=multiple
+make -j ${PROCS}
+make -j ${PROCS} install
 
 # environment variables are discovered later
